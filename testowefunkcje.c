47741aea558fa54cfd7365093b0477ea20da1c4e
DBrodowski19 <46489181+DBrodowski19@users.noreply.github.com>
2019-10-23 13:43:35 +0200

    Add files via upload

---
+++ testowefunkcje.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int a;
+int b;
+float add (float a; float b;){a+b;}
+float sub (float a; float b;){a-b;}
+float mul (float a; float b;) {a*b;}
+float div (float a; float b;) {a/b;}
+float sin (float a) {sin(a);}
+float cos (float b) {cos(b);}
+
+void test_add (void)
+{
+printf("Oczekiwano:" "Uzyskano:%f\n", add(2.1;2.0));
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+}
+void test_sub (void)
+{
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+}
+void test_mul (void)
+{
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+}
+void test_div (void)
+{
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+}
+
+void test_cos (void)
+{
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+printf("Oczekiwano:" "Uzyskano:");
+}
+void test_sin(void)
+{
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+    printf("Oczekiwano:" "Uzyskano:");
+
+    }
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+
+    }
+
+
+
