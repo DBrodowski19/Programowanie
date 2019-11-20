746d50336498291c072d74a31a893368f3f80ca9
DBrodowski19 <46489181+DBrodowski19@users.noreply.github.com>
2019-11-20 05:06:04 +0100

    Add files via upload

---
+++ hist.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define size 10
+int w;
+int main()
+
+{
+  int array[10];
+  int i, j, Najw, temp;
+for(w=0;w<10;w++)
+{
+scanf("%d",&array[w]);
+}
+
+  Najw = array[0];
+  for (i = 0; i < size; ++i)
+    if(array[i] > Najw)
+      Najw = array[i];
+  temp = Najw;
+  printf("\n\n");
+
+
+  for (i = 0; i < Najw; ++i) {
+    printf("\n%3d|", Najw - i);
+     for (j = 0; j < size; ++j)
+       if (array[j] < temp)
+     printf("   ");
+       else {
+     printf("  #");
+     --array[j];
+       }
+     --temp;
+  }
+
+//metryczka-x
+  printf("\n    ");
+  for (i = 0; i < size; ++i)
+    printf("  -", i);
+  printf("\n    ");
+  for (i = 0; i < size; ++i)
+    printf("%3d", i);
+  printf("\n");
+  return 0;
+}
